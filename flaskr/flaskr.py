60ce3d1f8cabd9d2c766e59793950fa9f83ccbc5
Kenneth Liu <ksliu25@gmail.com>
2016-08-18 23:51:42 -0500

    MVP for 1st step for now... able to upload an asset

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,15 @@
 import os
+import boto
+import botocore
 import sqlite3
+import hashlib
+import datetime
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
+
+from boto.s3.key import Key
+
+session 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -13,6 +21,9 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def create_s3_key_url(filename):
+    return hashlib.md5(filename + datetime.datetime.now().isoformat()).hexdigest()
 
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -46,18 +57,34 @@ def initdb_command():
 @app.route('/')    
 def slideshows():
     db = get_db()
-    cur = db.execute('select name, description, soundclip_s3_url from slideshows order by id desc')
+    cur = db.execute('select title, description, soundclip_s3_url from slideshows order by id desc')
     slideshows = cur.fetchall()
-    return render_template('slideshows.html', slideshows=slideshows)
+    return render_template('show_slideshows.html', slideshows=slideshows)
 
 @app.route('/', methods=['POST'])
 def add_slideshow():
     if not session.get('logged_in'):
-        abourt(401)
+        abort(401)
+
+    s3 = boto.connect_s3()
+    bucket_name = 'onein3million'
+    bucket = s3.get_bucket(bucket_name)
+    k = Key(bucket)
+
+    data_files = request.files.getlist('file[]')
+    for data_file in data_files:
+        file_contents = data_file.read()
+        #Hash for unique urls even if filenames are same. Defensive coding.
+        k.key = create_s3_key_url(data_file.filename)
+        print "Uploading some data to " + bucket_name + " with key " + k.key
+        k.set_contents_from_string(file_contents)
+    new_object_url = "https://s3-us-west-2.amazonaws.com/onein3million/" + k.key
+    print "WE'RR HERE!"
     db = get_db()
-    db.execute('insert into slideshows (name, description, soundclip_s3_url) values (?, ?, ?)', [request.form['name'], request.form['description'], request.form['soundclip_s3_url']])
+    db.execute('insert into slideshows (title, blurb, description, soundclip_s3_url) values (?, ?, ?, ?)', [request.form['title'], request.form['blurb'], request.form['description'], new_object_url])
     db.commit()
     flash('New slideshow added!')
+    print "ALMOST TO REDIRECT!"
     return redirect(url_for('slideshows'))
 
 @app.route('/login', methods=['GET', 'POST'])
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE if exists slideshows;
 CREATE TABLE slideshows (
   id integer primary key autoincrement,
-  name text not null,
+  title text not null,
   blurb text,
   description text,
   soundclip_s3_url text not null
@@ -11,8 +11,8 @@ DROP TABLE if exists photos;
 CREATE TABLE photos (
     id integer primary key autoincrement,
     slideshow_id integer not null,
-    name text not null,
+    title text not null,
     soundclip_range integer,
     photo_s3_url text not null,
     FOREIGN KEY(slideshow_id) REFERENCES slideshows(id)
-);
+);;
